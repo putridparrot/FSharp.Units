df8aab0ca09b5304c154d56a9611a929b3ed513f
Mark Timmings <mark.timmings@mtbsoftware.co.uk>
2017-05-10 21:32:02 +0100

    Added nuspec and AssemblyInfo with version info etc. in it.

---
+++ FSharp.Units/AssemblyInfo.fs
@@ -0,0 +1,16 @@
+﻿module AssemblyInfo
+
+open System.Reflection;
+
+[<assembly: AssemblyTitle("FSharp.Units")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("PutridParrot")>]
+[<assembly: AssemblyProduct("FSharp.Units")>]
+[<assembly: AssemblyCopyright("Copyright (c) PutridParrot 2017")>]
+[<assembly: AssemblyTrademark("")>]
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+()
