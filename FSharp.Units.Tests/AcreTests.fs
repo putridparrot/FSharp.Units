cfd70ed37edac951772af2da37669962dc115d74
Mark Timmings <mark.timmings@mtbsoftware.co.uk>
2021-10-11 20:23:06 +0100

    Added known value tests for Acre, also updated alg. as slightly out

--- FSharp.Units.Tests/AcreTests.fs
+++ FSharp.Units.Tests/AcreTests.fs
@@ -87,3 +87,52 @@ module AcreTests =
             |> should (equalWithin 0.01) value
     
         Check.QuickThrowOnFailure (testRange property)
+
+    [<TestCase(123<acre>, 49.7763)>]
+    [<TestCase(78.<acre>, 31.5655)>]
+    [<TestCase(0.9<acre>, 0.364217)>]
+    let ``Convert known Acre to Hectare`` (input, expectation) =
+        acre.toHectare input
+        |> should (equalWithin 0.01) expectation
+
+    [<TestCase(123<acre>, 497763.78)>]
+    [<TestCase(78.<acre>, 315655.08)>]
+    [<TestCase(0.9<acre>, 3642.174)>]
+    let ``Convert known Acre to Square Metre`` (input, expectation) =
+        acre.toSquareMetre input
+        |> should (equalWithin 0.01) expectation
+
+    [<TestCase(12<acre>, 522720)>]
+    [<TestCase(1.4<acre>, 60984.)>]
+    [<TestCase(0.9<acre>, 39204)>]
+    let ``Convert known Acre to Square Feet`` (input, expectation) =
+        acre.toSquareFeet input
+        |> should (equalWithin 0.01) expectation
+
+    [<TestCase(0.1<acre>, 627300.)>]
+    [<TestCase(6.<acre>, 37638000.)>]
+    [<TestCase(0.089<acre>, 558297.)>]
+    let ``Convert known Acre to Square Inch`` (input, expectation) =
+        acre.toSquareInch input
+        |> should (equalWithin 0.01) expectation
+
+    [<TestCase(123.<acre>, 0.497763)>]
+    [<TestCase(6.<acre>, 0.0242811)>]
+    [<TestCase(4660.<acre>, 18.85835)>]
+    let ``Convert known Acre to Square Kilometre`` (input, expectation) =
+        acre.toSquareKilometre input
+        |> should (equalWithin 0.01) expectation
+
+    [<TestCase(123.<acre>, 0.192188)>]
+    [<TestCase(6.<acre>, 0.009375)>]
+    [<TestCase(4660.<acre>, 7.28125)>]
+    let ``Convert known Acre to Square Mile`` (input, expectation) =
+        acre.toSquareMile input
+        |> should (equalWithin 0.01) expectation
+
+    [<TestCase(12.<acre>, 58080)>]
+    [<TestCase(0.9<acre>, 4356)>]
+    [<TestCase(45.<acre>, 217800)>]
+    let ``Convert known Acre to Square Yard`` (input, expectation) =
+        acre.toSquareYard input
+        |> should (equalWithin 0.01) expectation
