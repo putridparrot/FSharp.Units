f83f31f0a39267284784eb69089fa8a5fc6dc897
Mark Timmings <mark.timmings@mtbsoftware.co.uk>
2021-12-20 19:43:29 +0000

    Multi-targetting of net6, netstandard2.1 and net4.8 framework added as an alpha build.

--- FSharp.Units/AssemblyInfo.fs
+++ FSharp.Units/AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection;
 [<assembly: AssemblyCopyright("Copyright (c) PutridParrot 2021")>]
 [<assembly: AssemblyTrademark("")>]
 
-[<assembly: AssemblyVersion("1.0.1.0")>]
-[<assembly: AssemblyFileVersion("1.0.1.0")>]
+[<assembly: AssemblyVersion("1.0.2.0")>]
+[<assembly: AssemblyFileVersion("1.0.2.0")>]
 
 ()
